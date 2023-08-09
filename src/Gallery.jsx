b81ee85d28b3369668e9700c6b4c42a8661524da
amin-bani-a <amin.baniamerian67@gmail.com>
2023-08-09 23:36:44 +0400

    setup query

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,6 +1,19 @@
-import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+const url =
+  "https://api.unsplash.com/search/photos?client_id=ZjKgQpxdQu7JmXhVfscQlHJmfdKsaEjD1PDy6prj3eU&query=dog";
 
 const Gallery = () => {
+  const response = useQuery({
+    queryKey: ["images"],
+    queryFn: async () => {
+      const result = await axios.get(url);
+      console.log(response);
+      return result.data;
+    },
+  });
+
   return <h2>Gallery</h2>;
 };
 
